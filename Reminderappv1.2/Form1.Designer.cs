e8b7ba4bd5112dab8bb3260ca7d8967d2898273d
KIRAN RAJU <kraju49@gmail.com>
2018-05-28 21:30:59 +0530

    Reminder App
    Normal reminder app where you can add events to date and also receive notifications on time.

---
+++ Reminderappv1.2/Form1.Designer.cs
@@ -0,0 +1,279 @@
+﻿namespace Reminderappv1._2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.maskedTextBox1 = new System.Windows.Forms.MaskedTextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.maskedTextBox2 = new System.Windows.Forms.MaskedTextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.monthCalendar1 = new System.Windows.Forms.MonthCalendar();
+            this.label8 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(32, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 17);
+            this.label1.TabIndex = 0;
+            // 
+            // maskedTextBox1
+            // 
+            this.maskedTextBox1.Font = new System.Drawing.Font("Montserrat", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.maskedTextBox1.Location = new System.Drawing.Point(35, 104);
+            this.maskedTextBox1.Mask = "00:00:00";
+            this.maskedTextBox1.Name = "maskedTextBox1";
+            this.maskedTextBox1.Size = new System.Drawing.Size(186, 47);
+            this.maskedTextBox1.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(35, 67);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(43, 17);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Time";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Font = new System.Drawing.Font("Montserrat", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "AM",
+            "PM"});
+            this.comboBox1.Location = new System.Drawing.Point(227, 104);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(87, 48);
+            this.comboBox1.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(35, 251);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(68, 17);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Message";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Montserrat", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(35, 283);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(279, 49);
+            this.textBox1.TabIndex = 5;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Montserrat", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(35, 418);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(279, 41);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "SAVE TO DATABASE";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Montserrat", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(35, 465);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(279, 32);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "Minimize to tray";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // timer2
+            // 
+            this.timer2.Enabled = true;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.BalloonTipIcon = System.Windows.Forms.ToolTipIcon.Info;
+            this.notifyIcon1.BalloonTipText = "Reminder v1.2 is now minimized to system tray.";
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "Reminderv1.2";
+            this.notifyIcon1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseDoubleClick);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(35, 338);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(0, 17);
+            this.label4.TabIndex = 9;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(35, 158);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(41, 17);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Date";
+            // 
+            // maskedTextBox2
+            // 
+            this.maskedTextBox2.Font = new System.Drawing.Font("Montserrat", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.maskedTextBox2.Location = new System.Drawing.Point(38, 192);
+            this.maskedTextBox2.Mask = "00/00/00";
+            this.maskedTextBox2.Name = "maskedTextBox2";
+            this.maskedTextBox2.Size = new System.Drawing.Size(276, 47);
+            this.maskedTextBox2.TabIndex = 11;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(227, 67);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(58, 17);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "AM/PM";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(271, 9);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(50, 17);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "label7";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(438, 313);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(225, 32);
+            this.button2.TabIndex = 15;
+            this.button2.Text = "Show events";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // monthCalendar1
+            // 
+            this.monthCalendar1.Location = new System.Drawing.Point(438, 104);
+            this.monthCalendar1.MaxSelectionCount = 1;
+            this.monthCalendar1.Name = "monthCalendar1";
+            this.monthCalendar1.TabIndex = 14;
+            this.monthCalendar1.DateChanged += new System.Windows.Forms.DateRangeEventHandler(this.monthCalendar1_DateChanged);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(399, 383);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(418, 17);
+            this.label8.TabIndex = 16;
+            this.label8.Text = "Click on some date and then click above button to display";
+            this.label8.Click += new System.EventHandler(this.label8_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(817, 568);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.monthCalendar1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.maskedTextBox2);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.maskedTextBox1);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Montserrat", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Reminderv1.2";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.MaskedTextBox maskedTextBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.MaskedTextBox maskedTextBox2;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.MonthCalendar monthCalendar1;
+        private System.Windows.Forms.Label label8;
+    }
+}
+
---
+++ Reminderappv1.2/Form1.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace Reminderappv1._2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        string currentime,currentdate,messagetime,messagedate,new_msg,new_date,new_time,temptime;
+        SqlConnection con,icon,icon2;
+        SqlCommand cmd,icmd,icmd2;
+
+        private void label8_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string check_this = monthCalendar1.SelectionRange.Start.ToString("dd/MM/yy");
+            label8.Text = "";
+            icon2.Open();
+            icmd2 = new SqlCommand("select date,time,msg from data where date='" + check_this + "'");
+            icmd2.Connection = icon2;
+            rdr = icmd2.ExecuteReader();
+            while (rdr.Read())
+            {
+                new_date = rdr[0].ToString();
+                new_time = rdr[1].ToString();
+                new_msg = rdr[2].ToString();
+                label8.Text =label8.Text + "\n" + new_time + " " + new_msg;
+            }
+            icon2.Close();
+        }
+
+        SqlDataReader rdr;
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            currentime = DateTime.Now.ToString("hh:mm:ss tt");
+            currentdate = DateTime.Now.ToString("dd/MM/yy");
+            label1.Text = "Current time : " + currentime;
+            maskedTextBox2.Text = monthCalendar1.SelectionRange.Start.ToString("dd/MM/yy");
+        }
+
+        protected void Form1_Load(object sender, EventArgs e)
+        {
+            con = new SqlConnection("Data Source=.\\SQLEXPRESS;Initial Catalog=reminder;Integrated Security=true");
+            icon = new SqlConnection("Data Source=.\\SQLEXPRESS;Initial Catalog=reminder;Integrated Security=true");
+            icon2 = new SqlConnection("Data Source=.\\SQLEXPRESS;Initial Catalog=reminder;Integrated Security=true");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //timer2.Start();
+            //button1.Enabled = false;
+            con.Open();
+            cmd = new SqlCommand("insert into data values('" + maskedTextBox2.Text + "','" + messagetime + "','" + textBox1.Text + "')");
+            //button2.Enabled = true;
+            cmd.Connection = con;
+            int a = cmd.ExecuteNonQuery();
+            if (a > 0)
+            {
+                MessageBox.Show("Successfully uploaded");
+            }
+            else
+            {
+                MessageBox.Show("Upload failed");
+            }
+            con.Close();
+        }
+
+        public void monthCalendar1_DateChanged(object sender, DateRangeEventArgs e)
+        {
+            monthCalendar1.MaxSelectionCount = 1;
+        }
+
+        //private void button2_Click(object sender, EventArgs e)
+        //{
+        //timer2.Stop();
+        //button1.Enabled = true;
+        //button2.Enabled = false;
+        //label4.Text = " ";
+        //}
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            notifyIcon1.Visible = true;
+            this.Hide();
+            notifyIcon1.ShowBalloonTip(3000);
+        }
+
+        private void notifyIcon1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            this.Show();
+            notifyIcon1.Visible = false;
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            messagetime = maskedTextBox1.Text + " " + comboBox1.Text;
+            messagedate = maskedTextBox2.Text;
+            //label2.Text = messagetime;
+            label4.Text = "Set Reminder for:  " + messagetime;
+            label7.Text = "Today's date :"+currentdate;
+            icon.Close();
+            icon.Open();
+            //icmd = new SqlCommand("select date,time,msg from data");
+            icmd = new SqlCommand("select * from data where time='" + currentime + "'");
+            
+            icmd.Connection = icon;
+            rdr = icmd.ExecuteReader();
+            //int a1 = icmd.ExecuteNonQuery();
+            //if (a1 > 0)
+            //{
+            //    label9.Text = "something";
+            //    timer2.Stop();
+            //}
+            //else
+            //{
+            //    Random rnd = new Random();
+            //    int month = rnd.Next(1, 13);
+            //    label8.Text = "nothing"+ month;
+
+            //}
+            if (rdr.Read())
+            {
+                timer2.Interval = 1000;
+                //timer2.Stop();
+                temptime = currentime;
+                new_date = rdr[0].ToString();
+                new_time = rdr[1].ToString();
+                new_msg = rdr[2].ToString();
+                MessageBox.Show(new_msg, "Reminder");
+                
+            }
+            timer2.Interval = 100;
+            //icon.Close();
+            //timer2.Start();
+            /* if ( currentime == new_time && currentdate == new_date)
+             {
+             //timer2.Stop();
+                 MessageBox.Show("hehahah");
+                 button1.Enabled = true;
+                 //button2.Enabled = false;
+                 label4.Text = " ";
+             } */
+        }
+    }
+}
